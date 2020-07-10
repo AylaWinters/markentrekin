48bd754c7c0c88be7b1a796a1830961ddcfcdf87
Andre Entrekin <andre.entrekin@gmail.com>
2020-07-10 13:11:12 -0500

    nodemailer

--- public/mail.js
+++ public/mail.js
@@ -1,10 +1,11 @@
 const nodemailer = require("nodemailer");
 const mailgun = require("nodemailer-mailgun-transport");
+require("dotenv").config();
 
 const auth = {
   auth: {
-    api_key: "fb27bff36d26085218756be40a33d269-87c34c41-93da57a1",
-    domain: "sandbox8551a8e2c94c4bc68f8a8eebc1353ac3.mailgun.org",
+    api_key: process.env.MAILGUN_APIKEY,
+    domain: process.env.MAILGUN_DOMAIN,
   },
 };
 
